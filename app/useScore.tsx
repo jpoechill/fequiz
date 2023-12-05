5dcaf30c0c42a0dc73c617af8e1017543a355d63
jpoechill <poamrong@rith.com>
2023-12-05 00:40:26 -0800

    best bet

--- app/useScore.tsx
+++ app/useScore.tsx
@@ -16,35 +16,3 @@ function useScore() {
 }
 
 export default useScore;
-
-// const [local] = useState((): string => {
-//     if (typeof window !== 'undefined') {
-//         const from_localStorage = window.localStorage.getItem('theme')
-//         if (from_localStorage === null || from_localStorage === undefined) {
-//             return 'light'
-//         }
-
-//         return `${from_localStorage}` ? from_localStorage : 'light'
-//     }
-//     return ''
-// });
-// const [selected, setSelected] = useState<string>(local);
-// const [selectedOption, setSelectedOption] = useState<string>();
-
-// useEffect(() => {
-//     window.localStorage.setItem('theme', `${selected}`)
-//     const root = window.document.documentElement;
-//     const colorTheme = selected === "dark" ? "light" : "dark";
-//     root.classList.remove(colorTheme);
-//     root.classList.add(selected);
-
-//     setSelectedOption(`${selected}`);
-// }, [local, selected])
-
-// function handleChange() {
-//     if (selectedOption === 'light') {
-//         setSelected('dark')
-//     } else {
-//         setSelected('light')
-//     }
-// }
