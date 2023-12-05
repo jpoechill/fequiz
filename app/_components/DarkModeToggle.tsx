b6cd8375485d7ba534c23d08f77e280f22312bf6
jpoechill <poamrong@rith.com>
2023-12-04 23:56:34 -0800

    working localstorage updates

--- app/_components/DarkModeToggle.tsx
+++ app/_components/DarkModeToggle.tsx
@@ -1,27 +1,50 @@
 "use client"
 import React from 'react'
-import useDarkMode from "../useDarkMode";
 import Image from 'next/image'
+import { useEffect, useState } from "react";
 
 const DarkModeToggle = () => {
-    const [colorTheme, setTheme] = useDarkMode('light', 'theme');
+    const [local] = useState((): string => {
+        if (typeof window !== 'undefined') {
+            const from_localStorage = window.localStorage.getItem('theme')
+            if (from_localStorage === null || from_localStorage === undefined) {
+                return 'light'
+            }
+
+            return `${from_localStorage}` ? from_localStorage : 'light'
+        }
+        return ''
+    });
+    const [selected, setSelected] = useState<string>(local);
+    const [selectedOption, setSelectedOption] = useState<string>();
+
+    useEffect(() => {
+        window.localStorage.setItem('theme', `${selected}`)
+        const root = window.document.documentElement;
+        const colorTheme = selected === "dark" ? "light" : "dark";
+        root.classList.remove(colorTheme);
+        root.classList.add(selected);
+
+        setSelectedOption(`${selected}`);
+    }, [local, selected])
 
     function handleChange() {
-        if (colorTheme === "light") {
-            setTheme("light")
+        if (selectedOption === 'light') {
+            setSelected('dark')
         } else {
-            setTheme("dark")
+            setSelected('light')
         }
     }
-
     return (
         <div className='w-full flex items-center justify-end text-right mb-[40px] lrg:mb-[80px] h-[56px]'>
-            <Image src={colorTheme === "light" ? "/images/icon-sun-light.svg" : "/images/icon-sun-dark.svg"} className="inline" height="24" width="24" alt="HTML" />
+
+            {selectedOption}
+            <Image src={selectedOption === "dark" ? "/images/icon-sun-light.svg" : "/images/icon-sun-dark.svg"} className="inline" height="24" width="24" alt="HTML" />
             <label className="relative inline-flex items-center cursor-pointer mx-[16px]">
-                <input type="checkbox" checked={colorTheme === "dark" ? false : true} onChange={handleChange} className="sr-only peer" />
+                <input type="checkbox" checked={selectedOption === "light" ? false : true} onChange={handleChange} className="sr-only peer" />
                 <div className="w-12 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-[#A729F5]"></div>
             </label>
-            <Image src={colorTheme === "light" ? "/images/icon-moon-light.svg" : "/images/icon-moon-dark.svg"} className="inline" height="24" width="24" alt="HTML" />
+            <Image src={selectedOption === "dark" ? "/images/icon-moon-light.svg" : "/images/icon-moon-dark.svg"} className="inline" height="24" width="24" alt="HTML" />
         </div>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,20 +2,13 @@
 
 import Image from 'next/image'
 import Link from 'next/link';
-// import DarkModeToggle from './_components/DarkModeToggle'
-import { useEffect, useState } from "react";
+import DarkModeToggle from './_components/DarkModeToggle'
 
 export default function Home() {
 
-  // useEffect(() => {
-  //   if (typeof window !== "undefined") {
-  //     localStorage.setItem("score", "0");
-  //   }
-  // }, []);
-
   return (
     <main className="flex flex-col items-center justify-between pt-[20px] lg:pt-[80px] px-[25px] lg:w-[1160px] m-auto">
-      {/* <DarkModeToggle></DarkModeToggle> */}
+      <DarkModeToggle></DarkModeToggle>
       <div className='grid w-full lg:mt-[10px] lg:grid-cols-2'>
         <div className='text-left'>
           <h1 className='leading-tight dark-navy lg:text-[64px] dark:text-[#FFFFFF] lg:mb-[65px] mb-[25px]'>
@@ -74,7 +67,6 @@ export default function Home() {
         </div>
       </div>
     </main>
-
   )
 }
 
